16afa028e6e918109359c4856ea26ec7e5789281
Gurjeet singh CT-15 <91719457+Gurjeetsingh07@users.noreply.github.com>
2021-12-08 16:22:51 +0530

    Create Program1.py

---
+++ Program1.py
@@ -0,0 +1,7 @@
+# Q1. Display float number with 2 decimal places using print()
+
+a = 3.4536
+ 
+# using "%" to print value till 2 decimal places
+print ("The value of number till 2 decimal place(using %) is : ",end="")
+print ('%.2f'%a)
