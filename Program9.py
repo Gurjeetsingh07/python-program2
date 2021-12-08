02f8675b5996bdd212afeff645770e5e07031d37
Gurjeet singh CT-15 <91719457+Gurjeetsingh07@users.noreply.github.com>
2021-12-08 21:01:15 +0530

    Create Program9.py

---
+++ Program9.py
@@ -0,0 +1,9 @@
+#write a python program to calculate sum of three number if values are not equal return four time of there sum
+
+first = int(input("enter first number : "))
+second = int(input("enter second number : "))
+third = int(input("enter third number : "))
+fourth = int(input("enter fourth  number : "))
+numberList = [first, second, third, fourth]
+sum = numberList[0] + numberList[1] + numberList[2] + numberList[3]
+print(sum)
